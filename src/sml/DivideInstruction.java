f36a16bb4ff8fdf2565178dc09d64af8f0886a83
BBK-PiJ-2014-56 <BBK-PiJ-2014-56>
2015-02-08 18:11:54 +0000

    1st commit

--- src/sml/DivideInstruction.java
+++ src/sml/DivideInstruction.java
@@ -1,4 +1,4 @@
-package src.sml;
+package sml;
 
 /**
  * This class ....
@@ -6,17 +6,17 @@ package src.sml;
  * @author someone
  */
 
-public class MultiplyInstruction extends Instruction {
+public class DivideInstruction extends Instruction {
 
     private int result;
     private int op1;
     private int op2;
 
-    public MultiplyInstruction(String label, String op) {
+    public DivideInstruction(String label, String op) {
         super(label, op);
     }
 
-    public MultiplyInstruction(String label, int result, int op1, int op2) {
+    public DivideInstruction(String label, int result, int op1, int op2) {
         this(label, "mul");
         this.result = result;
         this.op1 = op1;
@@ -27,11 +27,15 @@ public class MultiplyInstruction extends Instruction {
     public void execute(Machine m) {
         int value1 = m.getRegisters().getRegister(op1);
         int value2 = m.getRegisters().getRegister(op2);
-        m.getRegisters().setRegister(result, value1 * value2);
+        try {
+            m.getRegisters().setRegister(result, value1 / value2);
+        } catch(Exception e) {
+            System.out.println("division by zero exception");
+        }
     }
 
     @Override
     public String toString() {
-        return super.toString() + " " + op1 + " * " + op2 + " to " + result;
+        return super.toString() + " " + op1 + " / " + op2 + " to " + result;
     }
 }
--- src/src/addTest.sml
+++
@@ -1,4 +0,0 @@
-f0 lin 20 6
-f1 lin 21 3
-f2 add 21 21 20
-f6 out 21
